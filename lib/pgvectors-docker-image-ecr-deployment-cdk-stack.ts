a60880114931585775355563429cf635c63e9d01
KennyDizi <xamarindevelopervietnam@outlook.com>
2023-11-30 06:43:31 +0700

    Update PgvectorsDockerImageEcrDeploymentCdkStack

--- lib/pgvectors-docker-image-ecr-deployment-cdk-stack.ts
+++ lib/pgvectors-docker-image-ecr-deployment-cdk-stack.ts
@@ -1,16 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ecrDeploy from 'cdk-ecr-deployment';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
 
 export class PgvectorsDockerImageEcrDeploymentCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        const repo = new ecr.Repository(this, 'PgvectorsDockerImageEcrRepository', {
+            repositoryName: 'pgvectorsrepository',
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PgvectorsDockerImageEcrDeploymentCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // Copy from docker registry to ECR.
+        new ecrDeploy.ECRDeployment(this, 'PgvectorsDockerImageECRDeployment', {
+            src: new ecrDeploy.DockerImageName('tensorchord/pgvecto-rs:pg16-latest'),
+            dest: new ecrDeploy.DockerImageName(`${repo.repositoryUri}:latest`),
+        });
+    }
 }
