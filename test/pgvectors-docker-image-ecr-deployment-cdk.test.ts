b8b5c6ebe5b57c570521f7d8984f22ad6308d857
KennyDizi <xamarindevelopervietnam@outlook.com>
2023-11-30 06:26:35 +0700

    Init project

---
+++ test/pgvectors-docker-image-ecr-deployment-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PgvectorsDockerImageEcrDeploymentCdk from '../lib/pgvectors-docker-image-ecr-deployment-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pgvectors-docker-image-ecr-deployment-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PgvectorsDockerImageEcrDeploymentCdk.PgvectorsDockerImageEcrDeploymentCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
