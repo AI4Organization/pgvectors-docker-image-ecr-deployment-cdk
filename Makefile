a2b9d5a2a28cd9a2239ad188d0d73dc37a29e443
KennyDizi <xamarindevelopervietnam@outlook.com>
2023-11-30 11:50:55 +0700

    More logging

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 .PHONY: cdk-deploy
 cdk-deploy:
 	cdk deploy -- --env-file .env
+
+help:
+    @echo "cdk-deploy - Deploy the CDK stack"
+    @echo "help       - Display this help message"
--- coreservices/init-db.sh
+++ coreservices/init-db.sh
@@ -4,3 +4,10 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     \i /docker-entrypoint-initdb.d/init.sql
 EOSQL
+
+if [ $? -eq 0 ]
+then
+  echo "Script ran successfully."
+else
+  echo "Script encountered an error." >&2
+fi
