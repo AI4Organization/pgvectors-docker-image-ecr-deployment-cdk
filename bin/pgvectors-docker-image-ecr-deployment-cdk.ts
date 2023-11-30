feef3db7f3c14eaed53c3b49c5a5bbe6436db669
KennyDizi <xamarindevelopervietnam@outlook.com>
2023-11-30 12:04:31 +0700

    Improve Dockerfile

--- bin/pgvectors-docker-image-ecr-deployment-cdk.ts
+++ bin/pgvectors-docker-image-ecr-deployment-cdk.ts
@@ -5,8 +5,9 @@ import * as cdk from 'aws-cdk-lib';
 import { PgvectorsDockerImageEcrDeploymentCdkStack } from '../lib/pgvectors-docker-image-ecr-deployment-cdk-stack';
 
 const app = new cdk.App();
+
+const { CDK_DEFAULT_ACCOUNT: account, CDK_DEFAULT_REGION: region } = process.env;
 new PgvectorsDockerImageEcrDeploymentCdkStack(app, 'PgvectorsDockerImageEcrDeploymentCdkStack', {
-    const { CDK_DEFAULT_ACCOUNT: account, CDK_DEFAULT_REGION: region } = process.env;
     env: {
         account,
         region
