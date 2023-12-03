7a757182218b45061f6736f3fd596531ffb27e2f
KennyDizi <xamarindevelopervietnam@outlook.com>
2023-12-03 20:19:54 +0700

    Fix runtime load env

--- lib/PgvectorsDockerImageEcrDeploymentCdkStackProps.ts
+++ lib/PgvectorsDockerImageEcrDeploymentCdkStackProps.ts
@@ -1,8 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
+import { IEnvTyped } from '../process-env-typed';
 
 export interface PgvectorsDockerImageEcrDeploymentCdkStackProps extends cdk.StackProps {
     readonly repositoryName: string;
     readonly appName: string;
     imageVersion?: string;
     environment?: string;
+    envTyped: IEnvTyped;
 }
--- process-env-typed.ts
+++ process-env-typed.ts
@@ -1,7 +1,7 @@
-export const EnvTyped = {
-    POSTGRES_PORT:process.env.POSTGRES_PORT ?? '5432',
-    POSTGRES_USER:process.env.POSTGRES_USER ?? 'postgres',
-    POSTGRES_PASSWORD:process.env.POSTGRES_PASSWORD ?? 'postgres',
-    POSTGRES_BASE_VERSION:process.env.POSTGRES_BASE_VERSION ?? '16.1',
-    POSTGRES_DB_NAME:process.env.POSTGRES_DB_NAME ?? 'hydra',
+export interface IEnvTyped {
+    POSTGRES_PORT: string;
+    POSTGRES_USER: string;
+    POSTGRES_PASSWORD: string;
+    POSTGRES_BASE_VERSION: string;
+    POSTGRES_DB_NAME: string;
 }
